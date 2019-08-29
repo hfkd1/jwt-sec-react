323862ad1dcac14d8e5ccd2bd49fb71277a9f5a5
Furkan Korkmaz <furkan.korkmaz@d1-tech.com>
2019-08-29 11:26:17 +0300

    added logout button

--- reactjs/src/Components/AppProfile.js
+++ reactjs/src/Components/AppProfile.js
@@ -23,13 +23,13 @@ export class AppProfile extends Component {
         </div>
         <label className="p-link layout-profile-link" onClick={this.onClick}>
           <span className="username">Claire Williams</span>
-          {/* <i className="pi pi-fw pi-cog" /> */}
-        </label>
-        {/* <ul className={classNames({'layout-profile-expanded': this.state.expanded})}>
+                    <i className="pi pi-fw pi-cog"/>
+                </button>
+                <ul className={classNames({'layout-profile-expanded': this.state.expanded})}>
                     <li><button className="p-link"><i className="pi pi-fw pi-user"/><span>Account</span></button></li>
                     <li><button className="p-link"><i className="pi pi-fw pi-inbox"/><span>Notifications</span><span className="menuitem-badge">2</span></button></li>
                     <li><button className="p-link"><i className="pi pi-fw pi-power-off"/><span>Logout</span></button></li>
-                </ul> */}
+                </ul>
       </div>
     );
   }
